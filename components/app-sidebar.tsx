c733ccc3b2b650d8d799b8c94f4e8cc8a905d4d2
Aditya Srivastava <102461506+AdityaSrivastava185@users.noreply.github.com>
2025-01-11 14:41:00 +0530

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -96,7 +96,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                   <GalleryVerticalEnd className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">VRV Security Dashboard</span>
+                  <span className="font-semibold">]Company Dashboard</span>
                   <span className="">v1.0.0</span>
                 </div>
               </a>
